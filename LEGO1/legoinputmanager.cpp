6fb94f007613e920b1d64775b7b76cb721482884
ecumber <ecumber05@gmail.com>
2023-07-03 13:48:46 -0700

    Move to new branch

--- LEGO1/legoinputmanager.cpp
+++ LEGO1/legoinputmanager.cpp
@@ -2,12 +2,32 @@
 
 #include "decomp.h"
 
-DECOMP_SIZE_ASSERT(LegoInputManager, 0x338); // 0x10059085
+//DECOMP_SIZE_ASSERT(LegoInputManager, 0x338); // 0x10059085
 
 // OFFSET: LEGO1 0x1005b790 STUB
 LegoInputManager::LegoInputManager()
 {
-  // TODO
+  m_unknown5C = 0;
+  m_unknown64 = NULL;
+  m_unknown60 = NULL;
+  m_unknown68 = 0;
+  m_unknown80 = 0;
+  m_timer = 0;
+  m_unknown6C = 0;
+  m_unknown70 = 0;
+  m_unknown84 = NULL;
+  m_unknown81 = 0;
+  m_unknown88 = 0;
+  m_directinputInterface = NULL;
+  m_directinputDeviceInterface = NULL;
+  m_unknown94 = 0;
+  m_unknown195 = 0;
+  m_joyid = (UINT)-1;
+  m_unknown19C = (UINT)-1;
+  m_joystickIndex = 0;
+  m_useJoystick = 0;
+  m_unknown74 = 0x19;
+  m_timeout = 1000;
 }
 
 // OFFSET: LEGO1 0x1005b8f0 STUB
@@ -35,10 +55,115 @@ void LegoInputManager::UnRegister(MxCore *)
 }
 
 // OFFSET: LEGO1 0x1005b8b0 STUB
-MxLong LegoInputManager::Tickle()
-{
-  // TODO
+//MxLong LegoInputManager::Tickle()
+//{
+//  // TODO
+//
+//  return 0;
+//}
 
-  return 0;
+// this function currently does not match 100% due to some member variables
+// being at the wrong offsets, but the functionality is the same
+
+// OFFSET: LEGO1 0x1005c030
+void LegoInputManager::CreateAndAcquireKeyboard(HWND hwnd)
+{
+  HINSTANCE hinstance = (HINSTANCE)GetWindowLongA(hwnd, GWL_HINSTANCE);
+  HRESULT hresult = DirectInputCreateA(hinstance, 0x500, &m_directinputInterface, NULL); // 0x500 for DX5
+  if (hresult == DI_OK)
+  {
+    HRESULT createdeviceresult = m_directinputInterface->CreateDevice(GUID_SysKeyboard, &m_directinputDeviceInterface, NULL);
+    if (createdeviceresult == DI_OK)
+    {
+      m_directinputDeviceInterface->RunControlPanel(hwnd, 6);
+      m_directinputDeviceInterface->SetDataFormat(&c_dfDIKeyboard);
+      m_directinputDeviceInterface->Acquire();
+    }
+  }
+  return;
 }
 
+int LegoInputManager::GetJoystickState(unsigned int* joystick_x, unsigned int* joystick_y, DWORD* buttons_state, unsigned int* pov_position)
+{
+  if (m_joystickIndex != JOYSTICKID1)
+  {
+    if (m_joyid < 0)
+    {
+      int result = GetJoystickId();
+      if (result == -1)
+      {
+        m_joystickIndex = 0;
+        return -1;
+      }
+    }
+
+    JOYINFOEX joyinfoex;
+    joyinfoex.dwSize = 0x34;
+    joyinfoex.dwFlags = JOY_RETURNX | JOY_RETURNY | JOY_RETURNBUTTONS;
+    UINT capabilities = m_joyCapsA.wCaps;
+    if ((capabilities & JOYCAPS_HASPOV) != 0) 
+    {
+      joyinfoex.dwFlags = JOY_RETURNX | JOY_RETURNY | JOY_RETURNPOV | JOY_RETURNBUTTONS;
+      if ((capabilities & JOYCAPS_POVCTS) != 0)
+      {
+          joyinfoex.dwFlags = JOY_RETURNX | JOY_RETURNY | JOY_RETURNPOV | JOY_RETURNBUTTONS | JOY_RETURNPOVCTS;
+      }
+    }
+    MMRESULT mmresult = joyGetPosEx(m_joyid, &joyinfoex);
+    if (mmresult == MMSYSERR_NOERROR) {
+      *buttons_state = joyinfoex.dwButtons;
+      UINT xmin = m_joyCapsA.wXmin;
+      UINT ymax = m_joyCapsA.wYmax;
+      UINT ymin = m_joyCapsA.wYmin;
+      *joystick_x = ((joyinfoex.dwXpos - xmin) * 100) / (m_joyCapsA.wXmax - xmin);
+      *joystick_y = ((joyinfoex.dwYpos - m_joyCapsA.wYmin) * 100) / (ymax - ymin);
+      if (m_joyCapsA.wCaps & (JOYCAPS_POV4DIR | JOYCAPS_POVCTS) == 0) {
+        *pov_position = (UINT)-1;
+        return 0;
+      }
+      if (joyinfoex.dwPOV == -1) {
+        *pov_position =  (UINT)-1;
+        return 0;
+      }
+      *pov_position = joyinfoex.dwPOV / 100;
+      return 0;
+    }
+  }
+  return -1;
+}
+
+// FIXME: there's 2 different variables for the joystick id??? also the lines that 
+// set dwSize and dwFlags result in incorrect offsets in the JOYINFOEX struct
+
+//  OFFSET: LEGO1 0x1005c240
+int LegoInputManager::GetJoystickId()
+{
+  JOYINFOEX joyinfoex;
+  if (m_joystickIndex != JOYSTICKID1)
+  {
+    UINT joyid = m_unknown19C;
+    if (-1 < (int)joyid)
+    {
+      joyinfoex.dwSize = 0x34;
+      joyinfoex.dwFlags = 0xFF;
+      MMRESULT result = joyGetPosEx(joyid, &joyinfoex);
+      if (result == JOYERR_NOERROR && (joyGetDevCapsA(joyid, &m_joyCapsA, 0x194) == JOYERR_NOERROR))
+      {
+        m_joyid = joyid;
+        return 0;
+      }
+    }
+    for (joyid = JOYSTICKID1; joyid < 0x10; joyid++)
+    {
+      joyinfoex.dwSize = 0x34;
+      joyinfoex.dwFlags = 0xFF;
+      MMRESULT result = joyGetPosEx(joyid, &joyinfoex);
+      if (result == JOYERR_NOERROR && (joyGetDevCapsA(joyid, &m_joyCapsA, 0x194) == JOYERR_NOERROR))
+      {
+        m_joyid = joyid;
+        return 0;
+      }
+    }
+  }
+  return -1;
+}
--- LEGO1/legoinputmanager.h
+++ LEGO1/legoinputmanager.h
@@ -3,6 +3,7 @@
 
 #include "decomp.h"
 #include "mxpresenter.h"
+#include <dinput.h>
 
 enum NotificationId
 {
@@ -25,13 +26,44 @@ public:
   __declspec(dllexport) void QueueEvent(NotificationId id, unsigned char p2, MxLong p3, MxLong p4, unsigned char p5);
   __declspec(dllexport) void Register(MxCore *);
   __declspec(dllexport) void UnRegister(MxCore *);
+  int GetJoystickState(unsigned int* something_x, unsigned int* something_y, DWORD* buttons_state, unsigned int* pov_position);
+  void CreateAndAcquireKeyboard(HWND hwnd);
+  int GetJoystickId();
 
-  virtual MxLong Tickle() override; // vtable+0x8
-
-  undefined m_pad40[0x15c];
-  int m_joystickIndex;
-  undefined m_pad200[0x194];
-  MxBool m_useJoystick;
+  //virtual MxLong Tickle() override; // vtable+0x8
+  MxCriticalSection m_criticalsection;                // 0x40
+  int m_unknown5C;                                    // 0x5C
+  int* m_unknown60;                                   // 0x60
+  int* m_unknown64;                                   // 0x64
+  int m_unknown68;                                    // 0x68
+  int m_unknown6C;                                    // 0x6C
+  int m_unknown70;                                    // 0x70
+  int m_unknown74;                                    // 0x74
+  UINT m_timer;                                       // 0x78
+  UINT m_timeout;                                     // 0x7C
+  char m_unknown80;                                   // 0x80
+  char m_unknown81;                                   // 0x81
+  char m_unknown82;                                   // 0x82
+  char m_unknown83;                                   // 0x83
+  int* m_unknown84;                                   // 0x84
+  char m_unknown88;                                   // 0x88
+  char m_unknown89[3];                                // 0x89
+  IDirectInputA* m_directinputInterface;              // 0x8C
+  IDirectInputDeviceA* m_directinputDeviceInterface;  // 0x90
+  char m_unknown94;                                   // 0x94
+  char m_unknown95[3];                                // 0x95
+  void* m_unknown98;                                  // 0x98
+  char m_unknown9C[0xF8];                             // 0x9C
+  MxBool m_unknown195;                                // 0x195
+  char m_unknown196[2];                               // 0x196
+  UINT m_joyid;                                       // 0x198
+  UINT m_unknown19C;                                  // 0x19C
+  JOYCAPSA m_joyCapsA;                                // 0x1a0
+  char m_joystickIndex;                               // 0x334
+  MxBool m_useJoystick;                               // 0x335
+  char m_unknown336;                                  // 0x336
+  char m_unknown337;                                  // 0x337
+  char m_unknown338;                                  // 0x338
 };
 
 #endif // LEGOINPUTMANAGER_H
